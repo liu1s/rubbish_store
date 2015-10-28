9a4694607fc18f881b2ea4b6035a180ad09fa002
liushuang <liu1s0404@outlook.com>
2015-10-28 10:03:22 +0800

    add expression test

--- go/study/expression.go
+++ go/study/expression.go
@@ -1,12 +1,20 @@
 package main
 
 //import "fmt"
-
 func main() {
-    s := "abc"
-    for i, n := 0, len(s); i < n; i++ {
-       println(s[i])
-    }
+	s := "abc"
+	for i, n := 0, len(s); i < n; i++ {
+		println(s[i])
+	}
+	a := "abc"
 
-
+	for j, k := range s {
+		println(j, k)
+	}
+	c := 1
+	d := 3
+	e := 4
+	f := 8
+	g = 1
+	m := map[string]int{"a": 1, "b": 2}
 }
