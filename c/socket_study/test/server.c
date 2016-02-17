2ef61ab230ea639b087a1aef06d807def62dd593
liushuang <liu1s0404@outlook.com>
2016-02-17 21:35:04 +0800

    add some socket study file

---
+++ c/socket_study/test/server.c
@@ -0,0 +1,59 @@
+/*
+#include    "unp.h"
+*/
+#include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <string.h>
+//#include <linux/in.h>
+#include <netinet/in.h>
+
+
+
+#define MAXLINE 1000
+#define LISTENQ 10
+#define SA struct sockaddr
+//#define INADDR_ANY "127.0.0.1"
+
+int
+main(int argc, char **argv)
+{
+    int                 listenfd, connfd;
+    socklen_t           len;
+    struct sockaddr_in  servaddr, cliaddr;
+    char                buff[MAXLINE];
+    time_t              ticks;
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family      = AF_INET;
+    /*servaddr.sin_addr.s_addr = htonl(INADDR_ANY);*/
+    //servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); //inet_addr 已被弃用
+    //inet_aton("127.0.0.1", servaddr.sin_addr.s_addr); //inet_pton的兼容性比inet_aton好
+    servaddr.sin_port = htons(7744); /* daytime server */
+
+    if (inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr) <= 0)
+            err_quit("inet_pton error for %s", argv[1]);
+
+
+    bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+
+    listen(listenfd, LISTENQ);
+
+    for ( ; ; ) {
+        len = sizeof(cliaddr);
+        connfd = accept(listenfd, (SA *) &cliaddr, &len);
+        printf("connection from %d, port %d\n",
+               inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)),
+               ntohs(cliaddr.sin_port));
+
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+        write(connfd, buff, strlen(buff));
+
+        close(connfd);
+    }
+}
---
+++ c/socket_study/test/test.sh
@@ -0,0 +1,4 @@
+gcc server.c changed_error.c -o server
+
+
+gcc client.c changed_error.c  -o client
